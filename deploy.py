fd96479591b22a340c93efd41983dd71431fe94f
dmytrolutsyk <49659495+dmytrolutsyk@users.noreply.github.com>
2021-02-01 21:41:21 +0100

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -11,7 +11,7 @@ def dropbox_upload(target_file_name, source_file, dropbox_token, dropbox_folder)
     dropbox_path = '/{folder}/{file_name}'.format(folder=dropbox_folder, file_name=target_file_name)
     dbx = dropbox.Dropbox(dropbox_token)
     data=open(source_file, 'rb')
-    dbx.files_upload(data.read(), target_file_name)
+    dbx.files_upload(data.read(), '/'+str(target_file_name))
 
 
 def get_app(release_dir):
