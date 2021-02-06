b989e028dbe3871cab82e86e7ee7575ec5042c7b
dmytrolutsyk <49659495+dmytrolutsyk@users.noreply.github.com>
2021-02-06 19:01:57 +0100

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -11,7 +11,7 @@ def dropbox_upload(target_file_name, source_file, dropbox_token, dropbox_folder)
     dropbox_path = '/{folder}/{file_name}'.format(folder=dropbox_folder, file_name=target_file_name)
     dbx = dropbox.Dropbox(dropbox_token)
     data=open(source_file, 'rb')
-    dbx.files_upload(data.read(), "/app.apk")
+    dbx.files_upload(data.read(), "/app.apk", files.WriteMode.update)
 
 
 def get_app(release_dir):
